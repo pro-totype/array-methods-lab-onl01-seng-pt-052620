decacd5bfc73244777345638f70b96921fddf3b1
Sean B <butts.sean01@gmail.com>
2020-06-09 02:38:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,15 @@
 def using_include(array, element)
+<<<<<<< HEAD
   array= ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include?(element)
+=======
+  using_include= ["wow", "I", "am", "really", "learning", "arrays!"]
+  element1= "wow"
+  element2= "Tardis"
+  using_include.include? ["wow", "I", "am", "really", "learning", "arrays!","element1"]
+  using_include.include? ("element2")
+
+>>>>>>> eff0a88b94119e8e3c6c6ef692219896c31835fc
 end
 
 
